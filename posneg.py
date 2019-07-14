97778b2e623fc94fe2faf473f5365fe85b6dc62d
vrashi <44301343+vrashi@users.noreply.github.com>
2019-07-14 11:24:43 +0530

    program to identify if a number is positive or negtive

---
+++ posneg.py
@@ -0,0 +1,8 @@
+#program to identify if a number is positive or negtive
+n = int(input("enter a number"))
+if n >= 1:
+    print("the number is positive")
+elif n <= -1:
+    print("the number is negtive")
+else:
+    print("the number is nither positive nor negtive")
