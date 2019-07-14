12d223af5ab52e0ad6ea7ff35f9821855b90d379
vrashi <44301343+vrashi@users.noreply.github.com>
2019-07-14 11:44:18 +0530

    program to find the aea of a triangle

---
+++ triangle.py
@@ -0,0 +1,4 @@
+#program to find the aea of a triangle
+base = int(input("enter the base of triangle"))
+height = int(input("enter the height of triangle"))
+print("area of the triangle is "+ str((1/2) * base * height))
