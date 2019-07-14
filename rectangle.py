59cc3798ae56989718226c073001a489c6ebe103
vrashi <44301343+vrashi@users.noreply.github.com>
2019-07-14 11:39:23 +0530

    program to find the area of recrangle

---
+++ rectangle.py
@@ -0,0 +1,4 @@
+#program to find the area of recrangle
+l = int(input("enter the length of the rectangle"))
+b = int(input("enter the breath of the rectangle"))
+print("area of the rectangle is "+ str(l*b))
